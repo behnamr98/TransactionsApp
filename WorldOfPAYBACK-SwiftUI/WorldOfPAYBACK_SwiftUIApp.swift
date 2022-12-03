6067c0eb555d4fb643a84184847d668e290a7e6f
Behnam <b.rakhshani77@gmail.com>
2022-12-03 14:09:43 +0330

    add SwiftUI Target for project

---
+++ WorldOfPAYBACK-SwiftUI/WorldOfPAYBACK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACK_SwiftUIApp.swift
+//  WorldOfPAYBACK-SwiftUI
+//
+//  Created by Behnam on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
